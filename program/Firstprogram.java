b22c871b8a25fc3e82ab2e528119f9bddf61d2a8
Sermakani-V <110027186+Sermakani-V@users.noreply.github.com>
2022-07-28 16:17:53 +0530

    Add files via upload

---
+++ program/Firstprogram.java
@@ -0,0 +1,15 @@
+class Firstprogram
+{
+  public static void main(String[]args)
+  {
+    System.out.println("Core Java");
+	System.out.println("12345");
+	System.out.println("10 + 20");
+	System.out.println("true"); //boolean
+	System.out.println("false"); //boolean
+	System.out.println("10.20f"); //floating
+	System.out.println("0b0011"); //integer
+	System.out.println(30+30+"javaprogram"+10+30);
+	System.out.println("End Program");
+   }
+ }

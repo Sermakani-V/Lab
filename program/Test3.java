f2926c1cded98dbd135f13633a26c0693f8d4354
Sermakani-V <110027186+Sermakani-V@users.noreply.github.com>
2022-08-04 18:03:59 +0530

    Add files via upload

---
+++ program/Test3.java
@@ -0,0 +1,25 @@
+class Test3
+{
+  public static void main(String[]args)
+   {
+   
+     int []arrValue= {20,30,40,50,60,70,80,90};
+	 
+	 System.out.println(arrValue[2]);
+	 System.out.println(arrValue[4]);
+	 
+	 int leng= arrValue.length;
+	 System.out.println(leng);
+	 
+	 for(int i=0; i<leng; i++)
+	 {
+		 System.out.println(arrValue[i]);
+	 }  
+	
+	 
+	}
+	 
+}
+	 
+	 
+	

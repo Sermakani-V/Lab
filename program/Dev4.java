53256bf5a9d8c72dc5ca82f6cbb5b3c3629528f1
Sermakani-V <110027186+Sermakani-V@users.noreply.github.com>
2022-08-04 21:10:06 +0530

    Add files via upload

---
+++ program/Dev4.java
@@ -0,0 +1,56 @@
+class Dev4
+{
+  public static void main(String[]args)
+   { 
+      int i=5, j=10, k=15;
+	  
+	  System.out.println("While loop values are: \n");
+	  
+	  while(i<k)                     //while loop
+	  { 
+        System.out.print(i + " "+ "\n");    
+		
+		i++;
+		
+	  }
+	  System.out.println("\n");
+	  System.out.println("do loop values are: ");
+	  
+	  do                            //do-while loop
+	  {
+		  System.out.print(j + "  \n");
+		  j++;
+	  }
+	  
+	  
+	  while(j!=15);
+	  {
+		  System.out.println("\n");
+		  System.out.println("do-while loop values are: ");
+		  System.out.println("invalid Numbers");
+	  }
+   }
+
+}
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+ 
+	
+	
